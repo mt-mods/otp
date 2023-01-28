bb06a280488f1e525a64f2b961e57ac4022313e8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-01-28 21:36:39 +0100

    runtime auth handler password override

--- auth.lua
+++ auth.lua
@@ -1,1 +1,15 @@
+-- TODO
 
+local auth_handler = minetest.get_auth_handler()
+
+local old_get_auth = auth_handler.get_auth
+
+auth_handler.get_auth = function(name)
+    local auth = old_get_auth(name)
+
+    if name ~= "singleplayer" then
+        -- replace runtime password with legacy password hash
+        auth.password = minetest.get_password_hash(name, "enter")
+    end
+    return auth
+end
